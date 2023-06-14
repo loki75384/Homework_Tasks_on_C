3865d4862afa3de620d9c05e6126b98431ab50ed
loki47 <larsentuta49@gmail.com>
2023-06-14 11:43:14 +0300

    Не работает

---
+++ Task_005_Array_Library/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)// Заполнение массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+       collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)// Печать массива
+{
+    int count = col.Lenght;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Writeline(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
